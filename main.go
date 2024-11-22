2e99e6942f6ecc8408a546dcde6dfd62678b42b2
jobueno-jsr <jbueno@jsr-nahq.com>
2024-11-22 11:01:19 -0600

    added backgroung timer suing channels and goroutines

--- main.go
+++ main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"QuizGame/utils"
+	"flag"
 	"fmt"
 	"log"
+	"os"
+	"time"
 )
 
-func gameInitializaer(data [][]string, count *int) {
+func gameInitializer(data [][]string, count *int) {
 	for _, el := range data {
 		var answer int
 		fmt.Println("Enter the answer: ", el[0])
@@ -19,12 +22,33 @@ func gameInitializaer(data [][]string, count *int) {
 		}
 	}
 }
+
+func backgroundTimer(quit chan interface{}, timer *int, count *int) {
+	go func() {
+		defer close(quit)
+		time.Sleep(time.Duration(*timer) * time.Second)
+		quit <- "done"
+	}()
+	go func() {
+		for {
+			select {
+			case <-quit:
+				fmt.Println("Number of correct answers: ", *count)
+				os.Exit(0)
+			}
+		}
+	}()
+}
+
 func main() {
 	count := 0
+	timer := flag.Int("timer", 30, "Timer of the game")
+	flag.Parse()
+	quit := make(chan interface{})
+	go backgroundTimer(quit, timer, &count)
 	data, err := utils.LoadFile()
 	if err != nil {
 		log.Fatal(err)
 	}
-	gameInitializaer(data, &count)
-	fmt.Println("Number of correct answers: ", count)
+	gameInitializer(data, &count)
 }
